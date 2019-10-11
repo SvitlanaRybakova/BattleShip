6d179061380f712d6c90cecd3a4cb6bbf73d4512
Svitlana <rsv0011@gmail.com>
2019-10-11 21:56:46 +0300

    Rename variable
    Change variable name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        BattleShipsProgram program = new BattleShipsProgram();
+        BattleShipsProgram newProgram = new BattleShipsProgram();
 
-        program.start();
+        newProgram.start();
 
     }
 }
